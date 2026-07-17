a1c42d5c74d43cc4630b097035f6a9630ce98c63
deepa <deepanshuyadav1811@gmail.com>
2026-07-17 11:24:24 +0530

    connected MongoDB in project

---
+++ backend/src/common/configs/db.config.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import 'dotenv/config';
+
+const connectDB = async () => {
+    try {
+        const connection = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB connected - connection object: ${connection}`);
+        console.log(`MongoDB connected: ${connection.connection.host}`);
+    }catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,11 @@
 import 'dotenv/config';
 import app from './app.js';
+import connectDB from './common/configs/db.config.js';
 
 const startServer = async () => {
     const PORT = process.env.PORT || 5000;
     //connect to DB
+    await connectDB();
 
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
