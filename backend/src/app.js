030944814d2a4366b75232c330ca638324c27c9c
deepa <deepanshuyadav1811@gmail.com>
2026-07-17 13:25:50 +0530

    setted up cors, morgan, cookie packages

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,22 @@
 import express from "express";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app  = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+app.use(cors({
+    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+    credentials: true, //so we can access cookie and other things
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"], //for recieving bearer token
+    
+}));
+
+app.use(morgan('dev'));
 
 export default app;
--- backend/src/common/configs/db.config.js
+++ backend/src/common/configs/db.config.js
@@ -4,7 +4,6 @@ import 'dotenv/config';
 const connectDB = async () => {
     try {
         const connection = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB connected - connection object: ${connection}`);
         console.log(`MongoDB connected: ${connection.connection.host}`);
     }catch (error) {
         console.error(`Error: ${error.message}`);
